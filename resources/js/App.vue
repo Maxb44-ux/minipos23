73366682c89036facd8009a25a8b188eef38c248
Maxb44-ux <sarttikounlartsavong@gmail.com>
2024-12-08 18:58:08 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <h1>App - Vue</h1>
+        <h2>lem</h2>
+        <p>Love Fight</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
